aae5de4757b5efd1793edc309a596b42571c6809
NoeliaChabiri <chabirinoelia@gmail.com>
2025-01-30 14:30:48 +0100

    cambios

--- C- ESTRUCTURAS CONDICIONALES/Ejercicio1-Condicionales-ChabiriNoelia.c
+++ C- ESTRUCTURAS CONDICIONALES/Ejercicio1-Condicionales-ChabiriNoelia.c
@@ -4,7 +4,7 @@
 
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
+int main()
 {
     float sueldo;
 
---
+++ ejemploArrays.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include <string.h> //cadena para trabajar con caracteres
+
+int main()
+
+{
+
+    float suma=0, media, max,min;
+    //Array tipo tabla - primera posicion 0 ultima 9.
+    float grupo[10];
+    float grupo2[10][10][10];//preguntar , declara la matri asi?
+    float grupo3
+
+    float grupo4[10,3];
+    int i;
+    grupo[0]=0;
+    grupo[1]=0;
+    grupo[2]=0;
+
+    grupo2[10][2]=5; // le damos un valor a la ubicacion aclarada en el codigo
+    grupo3[10,2]=5; 
+
+    return 0;
+
+}
