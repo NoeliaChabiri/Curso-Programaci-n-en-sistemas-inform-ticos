b02eead82af8cb5e7ecbb24bf198781a7fd7c06c
NoeliaChabiri <chabirinoelia@gmail.com>
2025-02-02 13:51:09 +0100

    cambios

--- Programación en C/3-ESTRUCTURAS ITERATIVAS/Ejercicio10-Iterativas-NoeliaChabiri.c
+++ Programación en C/3-ESTRUCTURAS ITERATIVAS/Ejercicio10-Iterativas-NoeliaChabiri.c
@@ -14,21 +14,28 @@
 
 int main()
 {
-    float numero;
+    float numero, parteDecimal;
     char paso;
-
+    int precision,parteEntera,aux,cociente, factor,binario;
+    precision=10;
+    aux=0;
+    factor=1;
+    binario=0;
     paso='c';
 
-    do
-    {
+    do{
 
-        printf ("\nPor favor ingrese un número decimal");
+        printf ("\nPor favor ingrese un número decimal: ");
         scanf("%f", &numero);
-        printf("\nA que lo quiere pasar?");
-        printf("\nSi lo quiere pasar a BINARIO ingrese B - ");
-        printf("Si lo quiere pasar a HEXAGECIMAL ingrese H: ");
+        printf("\nSi lo quiere pasar a BINARIO ingrese B  ");
+        printf("\nSi lo quiere pasar a HEXAGECIMAL ingrese H ");
+        printf("\nA que lo quiere pasar?: ");
         scanf(" %c", &paso);
         paso=toupper(paso);
+        parteEntera=(int)numero;
+        parteDecimal=numero-parteEntera;
+
+
 
         if (paso!='H'&&paso!='B')
         {
@@ -39,59 +46,67 @@ int main()
         // Pasamos número a binario
         if (paso=='B')
         {
-            float parteDecimal;
-            int precision,parteEntera,aux,cociente, factor,binario; // presicion son la cantidad de numeros luego del punto
-            precision=10;
-            aux=0;
-            factor=1;
-            binario=0;
-
-            printf("El número binario de %.3f es: " ,numero);
-            parteEntera=(int)numero; //asi solo lee la parte entera
-            parteDecimal=numero - parteEntera; //esta funcion solo toma la parte decimal.
-
-        while (parteEntera>0)
-        {
-            aux = parteEntera % 2;
-            binario += aux * factor; // Construimos el número binario, saca el numero final, moviendolos los da vuelta.
-            factor *= 10; // Multiplicamos por 10 para la siguiente posición
-            parteEntera /= 2; // Reducimos el número dividiéndolo por 2
-            printf("%d",aux);
-
-        }
-
-
-        if(parteDecimal>0)
-        { 
-            printf(".");  
+            if (parteEntera == 0) {
+                
+                printf("0");
+            }
             
-            while(precision>0 && parteDecimal>0)
-           {
-        
-            parteDecimal *= 2;  
-            aux = (int)parteDecimal;
-            printf("%d", aux);
-            parteDecimal -= aux;
-            precision--;
+            
+            //Saca resto y lo da vuelta.
+            while (parteEntera>0){
+
+                aux = parteEntera % 2;
+                binario += aux * factor; // Construimos el número binario, saca el numero final, moviendolos los da vuelta.
+                factor *= 10; // Multiplicamos por 10 para la siguiente posición
+                parteEntera /= 2; // Reducimos el número dividiéndolo por 2
 
             }
 
+             printf("\nEl número en binario es: %d",binario);
+
+            if(parteDecimal>0){
+                printf(".");  
+            }
+
+
+            while(precision>0 && parteDecimal>0){
+                parteDecimal *= 2;  
+                aux = (int)parteDecimal;
+                printf("%d", aux);
+                parteDecimal -= aux;
+                precision--;
+            }
+        
         }
 
+        //-------------------------------------------------------------------------------------------------------------------------
 
-        }        
-        
-
-        //Pasamos el numero a Hexagesimal
+        //Pasamos el numero a Hexagesimal - Saca resto y lo da vuelta.
         if (paso=='H')
         {
-            printf("El número en Hexagecimal es: %x", numero);
+            printf("El número en hexagecimal es: %X", parteEntera);
+
+
+            if(parteDecimal>0) { 
+                printf(".");  
+
+                while(precision>0 && parteDecimal>0)
+                {
+                    parteDecimal *= 16;  
+                    aux = (int)parteDecimal;
+                    printf("%X", aux); 
+                    parteDecimal -= aux;
+                    precision--;
+                
+                }
+
+    }
 
         }
         
     } while (paso!='H'&&paso!='B');
-    
-    
+
+    printf("\n");
 
     return 0;
 }
